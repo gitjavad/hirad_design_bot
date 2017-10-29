617459cb8eac1315bb86dcd58630c5d348301a8d
hirad <m.j.ahmadi88@gmail.com>
2017-10-29 20:10:54 +0330

    m

--- hirad_design_bot.php
+++ hirad_design_bot.php
@@ -55,7 +55,7 @@ if(isset($update->callback_query)){
     $chat_id = $update->callback_query->message->chat->id;
     $message_id = $update->callback_query->message->message_id;
     $tried = $update->callback_query->data+1;
-    $user=$update->callback_query->from->id;
+    $user=$update->callback_query->from->username;
     var_dump(
         makeHTTPRequest('editMessageText',[
             'chat_id'=>'@hirad_design_test',
