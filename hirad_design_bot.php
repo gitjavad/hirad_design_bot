fbd4e40a54f6a7063a4bbd608f8250c992c96276
hirad <m.j.ahmadi88@gmail.com>
2017-10-29 18:18:43 +0330

    m

--- hirad_design_bot.php
+++ hirad_design_bot.php
@@ -1,7 +1,17 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: MyMac
- * Date: 10/29/2017 AD
- * Time: 02:49
- */
+ini_set('error_reporting',E_ALL);
+$botToken = "477021117:AAElBQxFnLPV1j0C5SsthU6HwOeHdqkjeJ4";
+$website = "https://api.telegram.org/bot".$botToken;
+$update1 = file_get_contents('php://input');
+$update1 = str_replace('\n','%0A',$update1);
+$update = json_decode($update1, true);
+$chatid = $update['message']['chat']['id'];
+$username = $update["message"]["chat"]["username"];
+$photo_array = $update['message']['photo'];
+$video_file_id = $update['message']['video']['file_id'];
+$doc_file_id =  $update['message']['document']['file_id'];
+$message = $update['message']['text'];
+function sendmessage($message){
+    $url=$GLOBALS['website']."/sendMessage?chat_id=@btstest&text=".$message."&parse_mode=HTML";
+    file_get_contents($url);
+}
